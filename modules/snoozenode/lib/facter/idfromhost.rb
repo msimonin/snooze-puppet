3acc9d46189452b2b96ce5c03475d903b9e62596
Matthieu Simonin <matthieu.simonin@inria.fr>
2013-05-22 23:51:59 +0200

    fix facter to get an id compatible with port range ...
    M

--- modules/snoozenode/lib/facter/idfromhost.rb
+++ modules/snoozenode/lib/facter/idfromhost.rb
@@ -1,6 +1,6 @@
 Facter.add('idfromhost') do
   setcode do
     #string here
-    Facter.value(:ipaddress).split('.')[2] + Facter.value(:ipaddress).split('.')[3]
+    ((Facter.value(:ipaddress).split('.')[2] + Facter.value(:ipaddress).split('.')[3]).to_i % 60000).to_s
   end
 end
