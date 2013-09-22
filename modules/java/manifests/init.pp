ca6b7056e1c12a53b9d806762f6a72049426e163
msimonin <matthieu.simonin@inria.fr>
2013-09-22 12:43:17 +0200

    add pluginization add openjdk-6-sdk to java modules
    M

--- modules/java/manifests/init.pp
+++ modules/java/manifests/init.pp
@@ -2,4 +2,8 @@ class java{
    package { 'openjdk-6-jre':
      ensure => installed,
    }
+
+   package { 'openjdk-6-jdk':
+     ensure => installed,
+   }
 }
--- modules/snoozenode/manifests/init.pp
+++ modules/snoozenode/manifests/init.pp
@@ -1,25 +1,28 @@
-class snoozenode($type                                 = "bootstrap",
-                 $controlDataPort                      = 5000,
-                 $listenAddress                        = "localhost",
-                 $multicastAddress                     = "225.4.5.6",
-                 $groupManagerHeartbeatPort            = 9000,
-                 $zookeeperHosts                       = "localhost",
-                 $virtualMachineSubnet                 = "192.168.122.0/24",
-                 $externalNotificationHost             = "localhost",
-                 $databaseType                         = "memory",
-                 $databaseCassandraHosts               = "localhost:9160",
-                 $migrationMethod                      = "live",
-                 $migrationTimeout                     = 60,
-                 $reconfigurationEnabled               = false,
-                 $reconfigurationPolicy                = "Sercon",
-                 $reconfigurationInterval              = "0 0/1 *  * * ?",
-                 $energyManagementEnabled              = false,
-                 $energyManagementIdleTime             = 120,
-                 $energyManagementPowerSavingAction    = "suspendToRam",
-                 $energyManagementThresholdsWakeupTime = 300
-                 )
-                 {
-  
+class snoozenode(
+    $type                                 = "bootstrap",
+    $controlDataPort                      = 5000,
+    $listenAddress                        = "localhost",
+    $multicastAddress                     = "225.4.5.6",
+    $groupManagerHeartbeatPort            = 9000,
+    $zookeeperHosts                       = "localhost",
+    $virtualMachineSubnet                 = "192.168.122.0/24",
+    $externalNotificationHost             = "localhost",
+    $databaseType                         = "memory",
+    $databaseCassandraHosts               = "localhost:9160",
+    $migrationMethod                      = "live",
+    $migrationTimeout                     = 60,
+    $groupManagerSchedulerPluginsDirectory= "/usr/share/snoozenode/plugins/groupManagerScheduler",
+    $placementPolicy                      = "RandomScheduling",
+    $reconfigurationEnabled               = false,
+    $reconfigurationPolicy                = "Sercon",
+    $reconfigurationInterval              = "0 0/1 *  * * ?",
+    $energyManagementEnabled              = false,
+    $energyManagementIdleTime             = 120,
+    $energyManagementPowerSavingAction    = "suspendToRam",
+    $energyManagementThresholdsWakeupTime = 300
+    )
+{
+
   require 'java'
   require 'libvirt'
   require 'zookeeper'
@@ -42,7 +45,7 @@ class snoozenode($type                                 = "bootstrap",
 
   group { 'libvirt':
     ensure    => "present",
-    require  => Package["libvirt-bin"]
+              require  => Package["libvirt-bin"]
   }
 
   file { 'snoozeadmin-sudoers':
@@ -52,36 +55,36 @@ class snoozenode($type                                 = "bootstrap",
     group   => "root",
     mode  => '0440',
     source  => "puppet:///modules/snoozenode/etc/sudoers.d/snoozeadmin",
- }
+  }
 
 
   file { "/opt/snoozenode":
     ensure => "directory",
- }
+  }
 
- file { "/opt/snoozenode/snoozenode.deb":
-   ensure  => present,
-   owner   => root,
-   group   => root,
-   mode    => 644,
-   source  => "puppet:///modules/snoozenode/snoozenode.deb",
-   require => File['/opt/snoozenode'],
- }
+  file { "/opt/snoozenode/snoozenode.deb":
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => 644,
+    source  => "puppet:///modules/snoozenode/snoozenode.deb",
+    require => File['/opt/snoozenode'],
+  }
 
- package { 'snoozenode':
-   provider => dpkg,
-   source  => "/opt/snoozenode/snoozenode.deb",
-   require => [File["/opt/snoozenode/snoozenode.deb"],Package["openjdk-6-jre"]],
+  package { 'snoozenode':
+    provider => dpkg,
+    source  => "/opt/snoozenode/snoozenode.deb",
+    require => [File["/opt/snoozenode/snoozenode.deb"],Package["openjdk-6-jre"]],
   }
 
   file { 'snooze_node.cfg':
-  path    => '/usr/share/snoozenode/configs/snooze_node.cfg',
-  ensure  => file,
-  owner   => "root",
-  group   => "root",
-  mode  => '0644',
-  content => template("snoozenode/snooze_node.cfg.erb"),
-  require => Package["snoozenode"],
- }
+    path    => '/usr/share/snoozenode/configs/snooze_node.cfg',
+    ensure  => file,
+    owner   => "root",
+    group   => "root",
+    mode  => '0644',
+    content => template("snoozenode/snooze_node.cfg.erb"),
+    require => Package["snoozenode"],
+  }
 
 }
